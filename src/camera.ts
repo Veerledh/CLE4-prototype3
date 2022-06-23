1a52c37e844e54263ddd537b83fad2d115b932ba
Veerle <1037333@hr.nl>
2022-06-23 16:36:01 +0200

    first commit

---
+++ src/camera.ts
@@ -0,0 +1,50 @@
+import * as PIXI from 'pixi.js'
+import { AbstractRenderer, Application, Renderer } from 'pixi.js';
+
+import { Game } from './game';
+
+
+export class Camera {
+
+    pixi: PIXI.Application;
+    game: Game
+    
+    constructor(x: number, y: number, width: number, height: number, pixi: PIXI.Application, game: Game) {
+        
+        this.pixi = pixi
+        this.game = game
+
+        window.addEventListener("keydown", (e: KeyboardEvent) => this.onKeyDown(e))
+    }
+
+    onKeyDown(e:KeyboardEvent){
+        switch (e.key.toUpperCase()) {
+            case "C":
+                console.log('keydown');
+                this.takeScreenshot()
+
+        }
+    }
+    
+    action() {
+        console.log("klik")
+        this.takeScreenshot()
+    }
+
+    
+    takeScreenshot() {
+        let renderer: PIXI.Renderer = this.pixi.renderer as PIXI.Renderer
+        let extract = new PIXI.Extract(renderer)
+        let canvasPixels = extract.base64(this.pixi.stage)
+        // console.log(canvasPixels)
+
+        let sprite = PIXI.Sprite.from(canvasPixels)
+        sprite.x = 100
+        sprite.y = 100
+        sprite.width = 600
+        sprite.height = 300
+        this.pixi.stage.addChild(sprite)
+
+    }
+
+}
---
+++ src/game.ts
@@ -0,0 +1,47 @@
+import * as PIXI from 'pixi.js'
+import backgroundImage from "./images/background.png"
+import boer1Image from "./images/boer1.png"
+import tekstbox1Image from "./images/tekstbox1.png"
+import { Camera } from "./camera";
+
+export class Game{
+    
+    pixiWidth = 800;
+    pixiHeight = 450;
+    camera: Camera
+    pixi: PIXI.Application
+    tekstbox1Image: PIXI.Sprite
+    background:PIXI.Sprite
+
+    constructor(){
+    
+        this.pixi = new PIXI.Application({ width: this.pixiWidth, height: this.pixiHeight });
+        this.pixi.stage.interactive = true;
+        this.pixi.stage.hitArea = this.pixi.renderer.screen;
+        document.body.appendChild(this.pixi.view);
+        
+        this.pixi.loader
+            .add('backgroundTexture',backgroundImage)
+            .add('boer1Texture', boer1Image)
+            
+        this.pixi.loader.load(()=>this.doneLoading());
+
+        
+        
+    }
+    
+    doneLoading() {
+        this.background = new PIXI.Sprite(this.pixi.loader.resources["backgroundTexture"].texture!)
+        this.background.scale.set(1.32);
+
+        this.pixi.stage.addChild(this.background)
+        
+        this.camera = new Camera(-153,-418,120,282, this.pixi, this)
+        this.pixi.ticker.add((delta) => this.update(5));
+    }
+
+    update(delta: number) {
+    }
+}
+
+new Game();
